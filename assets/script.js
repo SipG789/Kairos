455792b5f451f4fcff58395ac29b480c88e432bf
SipG789 <94122022+SipG789@users.noreply.github.com>
2022-02-28 07:57:48 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,49 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+//using W3 schools to determine arrays and bootcamp 3.2.4
+const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+// using characters from keyboard that match owasp.org characters 
+const specialCharacter = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
 
 
 // TO DO: when button is clicked I am presented with prompts for password criteria 
 function generatePassword() {
-  //using W3 schools to determine arrays and bootcamp 3.2.4
-
 // TO DO: when prompted, I select which criteria to include, ask between 8-128 characters
   var generateBtn = prompt("Please select password length: between 8-128 characters.");
 // TO DO: Choose character types (lowercase, uppercase, numeric, special characters)
-
 
 if(generateBtn < 8 || generateBtn > 128) {
   window.alert("You need to provide a valid answer! Please try again.");
   return passwords;
 }
 
-
-if(generateBtn) {
+if(lowerCase) {
   window.confirm("Do you want to include lowercase?");  
-  window.confirm("Do you want to include UPPERCASE?");
-  window.confirm("Do you want to include special characters(!@#$%)?");
-  }    
-// I found a version of this code on https://stackoverflow.com/questions/1497481/javascript-password-generator and used it as a starting point with what I wanted to do. 
-// I modified the code to work with our criteria, I hope this is still considered my code. 
-  var length = 12,
-    lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    numeric = "1234567890";
-    specialCharacter = "~!@#$%^&*()_+";
-    passWord = "";
-  for (var i = 0; i <= length; i++) {
-    var randomLetter = Math.floor(Math.random() * lowerCase.length);
-    passWord += lowerCase.substring(randomLetter, randomLetter +1);
-    //passWord += lowerCase.lowerAt(Math.floor(Math.random() * lower));
-  for (var i = 0; i <= length; i++) {
-    var randomUpper = Math.floor(Math.random() * upperCase.length);
-    passWord += upperCase.substring(randomUpper, randomUpper + 1);
-  } 
-  for (var i = 0; i <= length; i++) {
-    var randomNum = Math.floor(Math.random() * numeric.length);
-    passWord += numeric.substring(randomNum, randomNum + 1);
-  }
-  for (var i =0; i <= length; i++) {
-    var randomChar = Math.floor(Math.random() * specialCharacter.length);
-    passWord += specialCharacter.substring(randomChar, randomChar + 1);
-  }
-  }
-  return passWord;
+  let lower = lowerCase.length;
+
+  for (let i= 0; i < lower; i++);
 }
 
+if (upperCase){
+  window.confirm("Do you want to include UPPERCASE?");
+  let upper = upperCase.length;
+
+  for (let i= 0; i < upper; i++);
+}
+if (specialCharacter) {
+  window.confirm("Do you want to include special characters(!@#$%)?");
+
+  let spec = specialCharacter.length;
+
+  for (let i= 0; i < spec; i++);
+  }
+
+  return Math.floor(Math.random()*12);
+}
 // TO DO: Generate password 
     // TO DO: Display an alert or write password on page 
-
 
 
 // Write password to the #password input
